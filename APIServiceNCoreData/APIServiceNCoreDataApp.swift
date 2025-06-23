103b344cfb60ef68540dc0a7ef3e359f9de9b35f
Pradyumna Rout <pradyumnarout1433@gmail.com>
2025-06-23 17:11:00 +0530

    Initial Commit

---
+++ APIServiceNCoreData/APIServiceNCoreDataApp.swift
@@ -0,0 +1,17 @@
+//
+//  APIServiceNCoreDataApp.swift
+//  APIServiceNCoreData
+//
+//  Created by hb on 23/06/25.
+//
+
+import SwiftUI
+
+@main
+struct APIServiceNCoreDataApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
